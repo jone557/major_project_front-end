7a1bc1bd71fc4c1c18b254ea31f56e8f3422c2c4
jone557 <ethiofanjo@gmail.com>
2023-04-05 15:58:56 +0530

    recommendation

--- src/Layers/Main/Main-home.js
+++ src/Layers/Main/Main-home.js
@@ -12,11 +12,13 @@ import { SingleUserRecommendation } from '../../Redux/reducers/userInteractionRe
 
 const MainHome = ()=>{
     const user = useSelector((state) => state.auth.user)
-    const priviousInteraction = useSelector((state) => state.userInteraction.interactions)
+    const priviousInteraction = useSelector((state) => state.userInteraction.recommendation)
+
     
-    // useEffect(()=>{
-    //     if(user) dispatch(SingleUserRecommendation(user.id))
-    // },[user])
+    useEffect(()=>{
+        dispatch(homeComponet())
+        if(user) dispatch(SingleUserRecommendation(user.id))
+    },[])
     const {searchRequest, error, isSearchLoading } = React.useContext(AppContext)
 
     const [cardData, setCardData] = useState(infoCards)
@@ -25,10 +27,14 @@ const MainHome = ()=>{
 
     
     const dispatch = useDispatch()
-    
-    useEffect(()=>{
-        dispatch(homeComponet())
-    },[])
+    function scrolll(){
+        const left = document.querySelector('.for_you_container');
+        if(left) left.scrollBy(-350, 0)        
+    }
+    function scrollr(){
+        const right = document.querySelector('.for_you_container');
+        if(right) right.scrollBy(350, 0);
+    }
 
     if(isLoading || isSearchLoading){
         return(
@@ -63,8 +69,34 @@ const MainHome = ()=>{
                     }
                 </div>
             </section>
+            
+            
 
+          {user? <section>
+            <div className='hdr-mrg'>
+                    <h1 className='hdr-title-marg'>For you</h1>
+            </div>
+            <div className="flx_container margin_top_4 margin_section">
+            <div className="arrow-box ">
+                <button id="Larrow" class="Larrow hov " onClick={scrolll}></button>
+            </div>
+                <div className='for_you_container'>
+                    {
+                        Array.isArray(priviousInteraction)? priviousInteraction.map((item)=>{
+                        // console.log(user)
+                            return <ComponentCard key={item.id} {...item}/>
+                        }) : <h1>{priviousInteraction}</h1>
+                    }
+                </div>
+               
+                <div className="arrow-box">
+                <button id="Rarrow" class="Rarrow hov " onClick={scrollr}></button>
+             </div>
+                </div>
 
+            </section>
+            : ''
+            }
             <section>
                 <div className="cards_container margin_top_4 margin_section">
                 <div className='grid g-3'>
